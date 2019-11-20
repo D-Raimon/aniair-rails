7d3e07c636c612e56d47218e86a71e3f9de62b64
D-Raimon <raimon67@gmail.com>
2019-11-20 17:04:58 -0500

    Modify files in spec folder
    - Removed unnecessary files that are not being used in the spec/features
    folder.
    - Modify factory_bot.rb to create a new instance of Show for show_spec.rb
    - Create a show_spec.rb to test the new Show Model.

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -7,4 +7,11 @@ FactoryBot.define do
     password_confirmation { 'password' }
   end
 
+  factory :show do
+    title { "Boku No Hero Academia" }
+    description { "4th Season of Boku No Hero Academia." }
+    image_url { "https://cdn.myanimelist.net/images/anime/1315/102961.jpg" }
+    trailer_url { "https://www.youtube.com/watch?v=pd6i3Kv2yIs" }
+  end
+
 end
