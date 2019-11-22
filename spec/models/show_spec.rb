e203e0ad352ce9bf62dcbdd3645d65936fd328a1
D-Raimon <raimon67@gmail.com>
2019-11-22 17:34:57 -0500

    Refactor test file for Show model
    - Add line to rails_helper that will allow the use of factory_bot in
    the spec files.
    - Refactor show_spec.rb to now make use of factory_bot.
    - Correct the name of the last field for Show from trialer_url to
    video_url.

--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -1,36 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Show, type: :model do
+  # let!(:user) { create(:user) }
 
-  subject { described_class.new }
-
-  it "is valid with valid attributes" do
-    subject.title = "Bleach"
-    subject.description = "Ichigo Kurosaki is an ordinary high schooler—until..."
-    subject.image_url = "www.something.com/this_is_a_thumbnail"
-    subject.video_url = "www.something.com/this_is_a_trailer"
-    expect(subject).to be_valid
-  end
-
-  it "is not valid without a title" do
-    expect(subject).to_not be_valid
-  end
-
-  it "is not valid without a description" do
-    subject.title = "Bleach"
-    expect(subject).to_not be_valid
-  end
-
-  it "is not valid without a start_date" do
-    subject.title = "Bleach"
-    subject.description = "Ichigo Kurosaki is an ordinary high schooler—until..."
-    expect(subject).to_not be_valid
-  end
-
-  it "is not valid without a end_date" do
-    subject.title = "Bleach"
-    subject.description = "Ichigo Kurosaki is an ordinary high schooler—until..."
-    subject.image_url = "www.something.com/this_is_a_thumbnail"
-    expect(subject).to_not be_valid
+  describe('validations') do
+    it('should create a valid show when required fields are complete') do
+      show = create(:show)
+      expect(show.save).to be(true)
+    end
   end
 end
