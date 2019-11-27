ed3190195a89c919865485bcaa1e554efd8ce194
D-Raimon <raimon67@gmail.com>
2019-11-27 15:45:34 -0500

    Create Subscription join table
    - Created a new join table Subscription to link Shows and Users.
        - Created a migration for Subscription, and added user and show id's
    - Created a subscription model to establish subscriptions relationship
    to Users and Shows.
    - Updated Show and User models to reflect their relationship through
    subscriptions.
    - Generation of new Subscription model also created a spec file for
    Subscription.
      - Tests will be added to the new spec files next.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_231758) do
+ActiveRecord::Schema.define(version: 2019_11_27_202654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2019_11_19_231758) do
     t.string "description", null: false
     t.string "image_url", null: false
     t.string "video_url", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "show_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
